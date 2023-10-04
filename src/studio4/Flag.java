c286c64561163cc9f70ee336c81d79a9405479f4
Steven Paine <Steven Paine@StevenPaine-PC.dhcp.wustl.edu>
2023-10-04 17:16:22 -0500

    Studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,20 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		File file = new File(Flag.class.getResource("resources/Images").getFile());
+		String[] imageList = file.list();
+		String imageName = imageList[0];
+		StdDraw.picture(0, 0, imageName);
 		
 	}
 }
